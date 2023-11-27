f40e057940d55cd21f3794e686eedc5e56d9f796
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-11-27 14:54:06 -0500

    Update Resp.

--- servicesComponents/ServiceCards.tsx
+++ servicesComponents/ServiceCards.tsx
@@ -21,7 +21,7 @@ const ServiceCards = () => {
                 <p className="text-3xl text-white  text-center pt-10">MOTOR VEHICLE ACCIDENTS</p>
                 <span className="border-dotted border-b-4 border-white    pt-5" />
                 <p className="text-2xl text-white  text-left pt-20 mx-auto pl-10 mx-auto   tracking-wide   ">We provide our clients with compassionate, yet aggressive representation  vehicle accidents.</p>
-                <p className="text-xl text-white  text-left pt-20    pl-10 tracking-wide text-left  p  ">Learn More</p>
+                <p className="text-xl text-white  text-left pt-20    pl-10 tracking-wide text-left  p  ">Learn More..</p>
             </div>
 
         </div>
